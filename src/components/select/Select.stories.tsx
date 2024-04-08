919980377b42b493cd9c32972ceaaabb331e733c
fireonex <mari.krulikovskaya@mail.ru>
2024-04-08 16:44:42 +0300

    commit 02-03.1

--- src/components/select/Select.stories.tsx
+++ src/components/select/Select.stories.tsx
@@ -1,23 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 import {Select} from "./Select";
-
+import {action} from "@storybook/addon-actions";
 
 
 export default {
     title: 'select',
-
+    component: Select
 }
 
+export const CustomSelect = () => {
 
-export const UncontrolledInput = () => {
+    const [value, setValue] = useState(2)
+
     return (
-        <Select value={'Choose name'} onChangeSelect={() => {}} items={
-            [
-                {id: 1, name: 'Sam'},
-                {id: 2, name: 'John'},
-                {id: 3, name: 'Jake'},
-                {id: 4, name: 'Van'}
-            ]
-        }/>
+        <Select onChangeSelect={setValue}
+                value={value}
+                items={[
+                    {value: 1, title: 'Sam'},
+                    {value: 2, title: 'John'},
+                    {value: 3, title: 'Jake'},
+                    {value: 4, title: 'Van'}
+                ]}
+        />
     )
 }
+
+
+export const CustomSelectWithoutValue = () => {
+
+    const [value, setValue] = useState(null)
+
+    return (
+        <Select onChangeSelect={setValue}
+                value={value}
+                items={[
+                    {value: 1, title: 'Sam'},
+                    {value: 2, title: 'John'},
+                    {value: 3, title: 'Jake'},
+                    {value: 4, title: 'Van'}
+                ]}
+        />
+    )
+}
+
+
+// export const CustomSelect = () => {
+//     return (
+//         <Select value={'Choose name'} onChangeSelect={() => {}} items={
+//             [
+//                 {id: 1, name: 'Sam'},
+//                 {id: 2, name: 'John'},
+//                 {id: 3, name: 'Jake'},
+//                 {id: 4, name: 'Van'}
+//             ]
+//         }/>
+//     )
+// }
--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,76 +1,177 @@
-import {useState} from "react";
+import {KeyboardEventHandler, useEffect, useState} from "react";
 import styled from "styled-components";
+import styles from './Select.module.css'
+import {KeyboardEvent} from "react";
 
-type ArrayType = {
-    id: any
-    name: string
+type ItemType = {
+    value: any
+    title: string
 }
 
 type SelectProps = {
-    value: any
+    value?: any
     onChangeSelect: (value: any) => void
-    items: Array<ArrayType>
-};
-
-type StyledDivProps = {
-    isFocused: boolean
+    items: Array<ItemType>
 };
 
 
 export const Select = (props: SelectProps) => {
 
-    const StyledDiv = styled.div<StyledDivProps>`
-        border: 2px solid ${props => (props.isFocused ? ' blue' : '')};
-        padding: 10px;
-        margin: 10px;
-        outline: none; /* Убираем стандартный outline при фокусе для красоты */
-        cursor: pointer; /* Чтобы показать, что это кликабельный элемент */
-    `;
+    const [active, setActive] = useState(false)
+    const [hoveredElementValue, setHoveredElementValue] = useState(props.value)
 
-    const DropdownItem = styled.div`
-        padding: 5px 10px;
-        cursor: pointer;
+    const SelectedItem = props.items.find(i => i.value === props.value)
+    const HoveredItem = props.items.find(i => i.value === hoveredElementValue)
 
-        &:hover {
-            background-color: lightblue;
+    useEffect(() => {
+        setHoveredElementValue(props.value)
+    }, [props.value]);
+
+    const ToggleItems = () => {
+        setActive(!active)
+    }
+
+    const onItemClick = (value: any) => {
+        props.onChangeSelect(value);
+        ToggleItems()
+    }
+
+    const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+            for (let i = 0; i < props.items.length; i++) {
+                if (props.items[i].value === hoveredElementValue) {
+                    const pretendentElement = e.key === 'ArrowDown'
+                        ? props.items[i + 1]
+                        : props.items[i - 1]
+
+                    if (pretendentElement) {
+                        props.onChangeSelect(pretendentElement.value)
+                        return;
+                    }
+                }
+
+            }
+            if (!SelectedItem) {
+                props.onChangeSelect(props.items[0].value)
+            }
         }
-    `;
-
-    const [selectValue, setSelectValue] = useState<any>(props.value)
-    const [isOpen, setIsOpen] = useState(false);
-    const [isFocused, setIsFocused] = useState(false);
-
-    const toggleDropdown = () => setIsOpen(!isOpen);
 
 
-    const onItemSelect = (item: ArrayType) => {
-        setSelectValue(item.id);
-        setIsOpen(false);
-        if (props.onChangeSelect) {
-            props.onChangeSelect(item.id);
+        if (e.key === 'Enter' || e.key === 'Escape') {
+            setActive(false)
         }
-    };
+
+        // const currentIndex = props.items.findIndex(item => item.value === hoveredElementValue);
+        // let newIndex = currentIndex;
+        //
+        // if (e.key === 'ArrowUp') {
+        //     newIndex = currentIndex <= 0 ? props.items.length - 1 : currentIndex - 1;
+        // } else if (e.key === 'ArrowDown') {
+        //     newIndex = currentIndex >= props.items.length - 1 ? 0 : currentIndex + 1;
+        // }
+        //
+        // const newHoveredValue = props.items[newIndex]?.value;
+        // setHoveredElementValue(newHoveredValue);
+        //
+        // // Если нужно, чтобы при нажатии клавиш выбирался элемент
+        // if (e.key === 'Enter' && newHoveredValue !== undefined) {
+        //     onItemClick(newHoveredValue);
+        // }
+    }
 
 
     return (
-        <div>
-            <StyledDiv onClick={toggleDropdown}
-                       onFocus={() => setIsFocused(true)}
-                       onBlur={() => setIsFocused(false)}
-                       isFocused={isFocused}
-            >
-                {selectValue}
-            </StyledDiv>
+        <div className={styles.select} onKeyUp={onKeyUp} tabIndex={0}>
+            <span onClick={ToggleItems}>{SelectedItem && SelectedItem.title}</span>
             {
-                isOpen &&
-                props.items.map(i =>
-                    <DropdownItem key={i.id} onClick={() => onItemSelect(i)}>
-                        {i.name}
-                    </DropdownItem>
-                )
+                active &&
+                <div className={styles.items}>
+                    {props.items.map(i =>
+                        <div onMouseEnter={() => {
+                            setHoveredElementValue(i.value)
+                        }}
+                             className={styles.item + " " + (HoveredItem === i ? styles.selected : '')}
+                             onClick={() => onItemClick(i.value)}
+                             key={i.value}>
+                            {i.title}
+                        </div>)}
+                </div>
             }
         </div>
-    );
+    )
 }
+
+
+// type ArrayType = {
+//     id: any
+//     name: string
+// }
+//
+// type SelectProps = {
+//     value: any
+//     onChangeSelect: (value: any) => void
+//     items: Array<ArrayType>
+// };
+//
+// type StyledDivProps = {
+//     isFocused: boolean
+// };
+//
+//
+// export const Select = (props: SelectProps) => {
+//
+//     const StyledDiv = styled.div<StyledDivProps>`
+//         border: 2px solid ${props => (props.isFocused ? ' blue' : '')};
+//         padding: 10px;
+//         margin: 10px;
+//         outline: none; /* Убираем стандартный outline при фокусе для красоты */
+//         cursor: pointer; /* Чтобы показать, что это кликабельный элемент */
+//     `;
+//
+//     const DropdownItem = styled.div`
+//         padding: 5px 10px;
+//         cursor: pointer;
+//
+//         &:hover {
+//             background-color: lightblue;
+//         }
+//     `;
+//
+//     const [selectValue, setSelectValue] = useState<any>(props.value)
+//     const [isOpen, setIsOpen] = useState(false);
+//     const [isFocused, setIsFocused] = useState(false);
+//
+//     const toggleDropdown = () => setIsOpen(!isOpen);
+//
+//
+//     const onItemSelect = (item: ArrayType) => {
+//         setSelectValue(item.id);
+//         setIsOpen(false);
+//         if (props.onChangeSelect) {
+//             props.onChangeSelect(item.id);
+//         }
+//     };
+//
+//
+//     return (
+//         <div>
+//             <StyledDiv onClick={toggleDropdown}
+//                        onFocus={() => setIsFocused(true)}
+//                        onBlur={() => setIsFocused(false)}
+//                        isFocused={isFocused}
+//             >
+//                 {selectValue}
+//             </StyledDiv>
+//             {
+//                 isOpen &&
+//                 props.items.map(i =>
+//                     <DropdownItem key={i.id} onClick={() => onItemSelect(i)}>
+//                         {i.name}
+//                     </DropdownItem>
+//                 )
+//             }
+//         </div>
+//     );
+// }
 
 
